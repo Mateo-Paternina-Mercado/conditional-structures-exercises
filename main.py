231e7487b532849f466794742fd167b91e45ca7c
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-03 17:39:25 -0500

    Ejercicio de Division

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
-#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más. Más precisamente, la diferencia es de más o menos un cuarto de día.
+#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
 
-#Para evitar que las estaciones se desfasen con el calendario, el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
+#Dividendo: 14
+#Divisor: 5
+#La división no es exacta.
+#Cociente: 2
+#Resto: 4
 
-#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
+#Dividendo: 100
+#Divisor: 10
+#La división es exacta.
+#Cociente: 10
+#Resto: 0
 
-#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que fuera divisible por 400.
+# Solicitar al usuario que ingrese el dividendo y el divisor
+dividendo = int(input("Dividend: "))
+divisor = int(input("Divider: "))
 
-#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+# Calcular el cociente y el resto
+cociente = dividendo // divisor
+resto = dividendo % divisor
 
-#Ingrese un anno: 1988
-#1988 es bisiesto
-#Ingrese un anno: 2011
-#2011 no es bisiesto
-#Ingrese un anno: 1700
-#1700 no es bisiesto
-#Ingrese un anno: 1500
-#1500 es bisiesto
-#Ingrese un anno: 2400
-#2400 es bisiesto
-
-# Solicitar al usuario que ingrese un año
-año = int(input("Enter a year: "))
-
-# Determinar si el año es bisiesto
-if año < 1582:
-    # Regla del calendario juliano
-    if año % 4 == 0:
-        print(f"{año} It's a leap year")
-    else:
-        print(f"{año} it's not a leap year")
+# Determinar si la división es exacta
+if resto == 0:
+    print("The division is exact.")
 else:
-    # Regla del calendario gregoriano
-    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
-        print(f"{año} It's a leap year")
-    else:
-        print(f"{año} it's not a leap year")
+    print("The division is not exact.")
+
+# Mostrar el cociente y el resto
+print(f"Quotient: {cociente}")
+print(f"Rest: {resto}")
