3393dcbb9b10ee375f1315f3263c0fb37a3621e2
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-03 19:18:07 -0500

    Ejercicio ordenamiento de 2 numeros

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
+#Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
+#Ingrese numero: 51
+#Ingrese numero: 24
+#24 51
+#A continuación, escriba otro programa que haga lo mismo con tres números:
+#Ingrese numero: 8
+#Ingrese numero: 1
+#Ingrese numero: 4
+#1 4 8
+#Finalmente, escriba un tercer programa que ordene cuatro números:
+#Ingrese numero: 7
+#Ingrese numero: 0
+#Ingrese numero: 6
+#Ingrese numero: 1
+#0 1 6 7
+#Recuerde que su programa debe entregar la solución correcta para cualquier combinación de números, no sólo para los ejemplos mostrados aquí.
+#Hay más de una manera de resolver cada ejercicio.
 
-#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
-#Palabra 1: edificio
-#Palabra 2: tren
-#La palabra edificio tiene 4 letras mas que tren.
-#Palabra 1: sol
-#Palabra 2: paralelepipedo
-#La palabra paralelepipedo tiene 11 letras mas que sol
-#Palabra 1: plancha
-#Palabra 2: lapices
-#Las dos palabras tienen el mismo largo
+# Solicitar al usuario que ingrese dos números
+numero1 = int(input("Enter number: "))
+numero2 = int(input("Enter number: "))
 
-# Solicitar al usuario que ingrese dos palabras
-palabra1 = input("word 1: ")
-palabra2 = input("word 2: ")
-
-# Calcular la longitud de ambas palabras
-longitud1 = len(palabra1)
-longitud2 = len(palabra2)
-
-# Comparar las longitudes y mostrar el resultado
-if longitud1 > longitud2:
-    diferencia = longitud1 - longitud2
-    print(f"the word {palabra1} has {diferencia} letters more than {palabra2}.")
-elif longitud2 > longitud1:
-    diferencia = longitud2 - longitud1
-    print(f"the word {palabra2} has {diferencia} letters more than {palabra1}.")
+# Ordenar los números y mostrarlos
+if numero1 < numero2:
+    print(numero1, numero2)
 else:
-    print("The two words are the same length.")
+    print(numero2, numero1)
