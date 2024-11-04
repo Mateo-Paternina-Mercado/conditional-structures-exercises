ed68d35949b852faa63370ef4b17fe3805984e85
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-03 19:08:58 -0500

    Ejercico palabra mas larga

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
 
-#Dividendo: 14
-#Divisor: 5
-#La división no es exacta.
-#Cociente: 2
-#Resto: 4
+#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
+#Palabra 1: edificio
+#Palabra 2: tren
+#La palabra edificio tiene 4 letras mas que tren.
+#Palabra 1: sol
+#Palabra 2: paralelepipedo
+#La palabra paralelepipedo tiene 11 letras mas que sol
+#Palabra 1: plancha
+#Palabra 2: lapices
+#Las dos palabras tienen el mismo largo
 
-#Dividendo: 100
-#Divisor: 10
-#La división es exacta.
-#Cociente: 10
-#Resto: 0
+# Solicitar al usuario que ingrese dos palabras
+palabra1 = input("word 1: ")
+palabra2 = input("word 2: ")
 
-# Solicitar al usuario que ingrese el dividendo y el divisor
-dividendo = int(input("Dividend: "))
-divisor = int(input("Divider: "))
+# Calcular la longitud de ambas palabras
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
 
-# Calcular el cociente y el resto
-cociente = dividendo // divisor
-resto = dividendo % divisor
-
-# Determinar si la división es exacta
-if resto == 0:
-    print("The division is exact.")
+# Comparar las longitudes y mostrar el resultado
+if longitud1 > longitud2:
+    diferencia = longitud1 - longitud2
+    print(f"the word {palabra1} has {diferencia} letters more than {palabra2}.")
+elif longitud2 > longitud1:
+    diferencia = longitud2 - longitud1
+    print(f"the word {palabra2} has {diferencia} letters more than {palabra1}.")
 else:
-    print("The division is not exact.")
-
-# Mostrar el cociente y el resto
-print(f"Quotient: {cociente}")
-print(f"Rest: {resto}")
+    print("The two words are the same length.")
